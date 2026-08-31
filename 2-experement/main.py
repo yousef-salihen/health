f8676203ed69c1443ebe22465117892496b0610d
yousef salihen <you12817@icloud.com>
2026-08-31 16:04:31 +0300

    Update main.py

--- 2-experement/main.py
+++ 2-experement/main.py
@@ -2,7 +2,13 @@ from model.preprocessing import load_data, handle_missing_values, check_correlat
 from model.model import train_logistic_regression, evaluate_model
 
 def main():
-    print("Hello, World!")
+    # load the dataset
+    # clean the dataset
+    # fill in miss values
+    # correlation Analysis
+    # normalization
+    # classification Adaline model
+    print('hello worlld')
 
 if __name__ == "__main__":
     main()
